1a478ae260a483e20eae459d1c9fbacd344f33d4
pepure <pepure@163.com>
2019-12-13 23:22:49 +0800

    test update

---
+++ helloworld.py
@@ -0,0 +1,24 @@
+#print("hello world*********************")
+
+#print(2)
+print("678*999")
+
+a="apple"
+print(a)
+
+myteacher="mr.chen"
+print("myteacher")
+
+a=100
+print(a)
+
+
+b=200
+c=a+b
+print(c)
+
+print(a,"+",b,"=",c)
+
+
+
+print(123)
