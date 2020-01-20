34bcb102f772eff78a45bc0c70f82a90244a9cea
Oceania2018 <haiping008@gmail.com>
2020-01-20 01:33:47 -0600

    upgrade SharpCV to 0.3.0.

--- src/SharpCV.Exmaples/InferingInTensorflow.cs
+++ src/SharpCV.Exmaples/InferingInTensorflow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NumSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using static SharpCV.Binding;
@@ -12,15 +13,30 @@ namespace SharpCV.Exmaples
     {
         public bool Run()
         {
-            /*var net = cv2.dnn.readNetFromTensorflow(@"D:\mask_rcnn_inception_v2_coco_2018_01_28\frozen_inference_graph.pb",
+            var net = cv2.dnn.readNetFromTensorflow(@"D:\mask_rcnn_inception_v2_coco_2018_01_28\frozen_inference_graph.pb",
                 @"D:\mask_rcnn_inception_v2_coco_2018_01_28\mask_rcnn_inception_v2_coco_2018_01_28.pbtxt");
-            
-            var image = cv2.imread(@"D:\SciSharp\SciSharp-Stack-Examples\data\images\cars.jpg");
-            var blob = cv2.dnn.blobFromImage(image, 1.0, size: (300, 300), swapRB: true, crop: false);
-
+            var img = cv2.imread(@"D:\SciSharp\SciSharp-Stack-Examples\data\images\cars.jpg");
+            var rows = img.shape[0];
+            var cols = img.shape[1];
+            var blob = cv2.dnn.blobFromImage(img, 1.0, size: (300, 300), swapRB: true, crop: false);
             net.setInput(blob);
-            var output = net.forward();*/
+            NDArray output = net.forward();
 
+            foreach (var detection in output[0, 0, Slice.All, Slice.All].GetNDArrays())
+            {
+                var score = detection.GetSingle(2);
+                if(score > 0.3)
+                {
+                    var left = detection.GetSingle(3) * cols;
+                    var top = detection.GetSingle(4) * rows;
+                    var right = detection.GetSingle(5) * cols;
+                    var bottom = detection.GetSingle(6) * rows;
+                    cv2.rectangle(img, ((int)left, (int)top), ((int)right, (int)bottom), (23, 230, 210), thickness: 2);
+                }
+            }
+
+            cv2.imshow("img", img);
+            cv2.waitKey();
             return true;
         }
     }
